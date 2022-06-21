e15445f53fe29ef5d90d79d88bc498e366a5c740
Alberto Basalo <albertobasalo@hotmail.com>
2022-06-21 11:34:00 +0200

    feat: child

---
+++ src/7-child/child.js
@@ -0,0 +1,12 @@
+console.log("child started working");
+
+process.send({ msg: "I am a child working" });
+process.on("message", (arg) => {
+  if (arg.msg) {
+    console.log(`Received from parent ${arg.msg}`);
+  }
+  if (arg.close) {
+    console.log("parent make me close");
+    process.exit(1);
+  }
+});
