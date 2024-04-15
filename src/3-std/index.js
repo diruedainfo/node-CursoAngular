da56d874f7a6ecd80f785c4d56dce562fa3bfaa9
drueda <diego.rueda@igmweb.com>
2024-04-15 15:54:28 +0200

    feat: 3-std std in std out

--- src/3-std/index.js
+++ src/3-std/index.js
@@ -1,5 +1,8 @@
 process.stdout.write("Hola mundo\n");
 
+// const escrito = process.stdin.read();
+// process.stdout.write(`Hola ${escrito}`);
+
 process.stdin.on("readable", () => {
   const escrito = process.stdin.read();
   process.stdout.write(`Hola ${escrito}`);
